4a77697c21802c55e27ecc73611530f2bb371215
Priyanshuprinci <princi.it.js@gmail.com>
2024-05-07 20:47:48 +0530

    changing server file

--- server.js
+++ server.js
@@ -198,8 +198,10 @@ App.post("/api/create-checkout-session", async function (req, res) {
         payment_method_types: ["card"],
         line_items: lineItems,
         mode: "payment",
-        success_url: `http://localhost:3000/success`,
-        cancel_url: `http://localhost:3000/cancel`
+        //  success_url: `http://localhost:3000/success`,
+        //  cancel_url: `http://localhost:3000/cancel`
+        success_url:`https://ecommerce-frontend-njbl.onrender.com/success`,
+        cancel_url:`https://ecommerce-frontend-njbl.onrender.com/cancel`
     })
     res.json({ id: session.id }) 
  
